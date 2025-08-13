36a7f37a796072b00a935dd49679481b990365f3
efeovez <efeovez2010@gmail.com>
2025-08-13 21:45:31 +0300

    add tokens and token interface

---
+++ contracts/token/IBasisToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IERC20} from "../libs/IERC20.sol";
+
+interface IBasisToken is IERC20 {
+
+    /* ================= FUNCTIONS ================= */
+
+    function mint(address account, uint256 amount) external returns (bool);
+
+    function burn(uint256 amount) external;
+
+    function burnFrom(address from, uint256 amount) external;
+
+    function isGovernor() external returns (bool);
+
+    function governor() external view returns (address);
+}
