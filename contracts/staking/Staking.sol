bac92aba76bd2051aa8a2e37a71934f868386d35
efeovez <efeovez2010@gmail.com>
2025-08-14 10:15:31 +0300

    add delegate() function

--- contracts/staking/Staking.sol
+++ contracts/staking/Staking.sol
@@ -1,15 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {IERC20} from "../libs/IERC20.sol";
+import {SafeERC20} from "../libs/SafeERC20.sol";
 import {StakingState} from "./StakingState.sol";
 
 contract Staking is StakingState {
 
+    /* ================= STATE VARIABLES ================= */
+
+    using SafeERC20 for IERC20;
+
+    IERC20 public basis;
+
+    /* ================= CONSTRUCTOR ================= */
+
+    constructor(address basisAddress) {
+        basis = IERC20(basisAddress);
+    }
+
     /* ================= FUNCTIONS ================= */
 
-    function createProvider(string memory description_,
-                            uint8 commission_
-    ) public {
+    function createProvider(string memory description_, uint8 commission_) public {
         require(bytes(description_).length < 50, "basis.staking.Staking: description_ must be under 50 characters");
         require(commission_ <= 100, "basis.staking.Staking: commission_ must not exceed 100");
         require(providers[msg.sender].providerAddress == address(0), "basis.staking.Staking: provider already exists");
@@ -20,6 +32,30 @@ contract Staking is StakingState {
             commission: commission_,
             power: 0
         });
+    }
 
+    function editProvider(string memory description_, uint8 commission_) public {
+        require(bytes(description_).length < 50, "basis.staking.Staking: description_ must be under 50 characters");
+        require(commission_ <= 100, "basis.staking.Staking: commission_ must not exceed 100");
+        require(msg.sender == providers[msg.sender].providerAddress, "basis.staking.Staking: provider not registered");
+
+        Provider storage currentProvider = providers[msg.sender];
+
+        currentProvider.description = description_;
+        currentProvider.commission = commission_;
+    }
+
+    function delegate(address provider, uint256 amount) public {
+        require(basis.allowance(msg.sender, address(this)) >= amount, "basis.staking.Staking: approved amount is not sufficient");
+        require(providers[provider].providerAddress != address(0), "basis.staking.Staking: provider not registered");
+
+        basis.safeTransferFrom(msg.sender, address(this), amount);
+
+        Provider storage currentProvider = providers[provider];
+        currentProvider.power += amount;
+
+        Delegation storage userDelegation = delegations[msg.sender][provider];
+        userDelegation.amount += amount;
+        userDelegation.unlockTime = block.timestamp + LOCK_PERIOD;
     }
 }
--- contracts/staking/StakingState.sol
+++ contracts/staking/StakingState.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 contract StakingState {
 
-    /* ================= STRUCT ================= */
+    /* ================= STRUCTS ================= */
 
     struct Provider {
         address providerAddress;
@@ -12,9 +12,18 @@ contract StakingState {
         uint256 power;
     }
 
+    struct Delegation {
+    uint256 amount;
+    uint256 unlockTime;
+    }
+
     /* ================= STATE VARIABLES ================= */
 
     mapping(address => Provider) public providers;
+
+    mapping(address => mapping(address => Delegation)) public delegations;
+
+    uint256 public constant LOCK_PERIOD = 21 days;
 
     uint256 public totalStaked;
 }
