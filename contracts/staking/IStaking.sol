8e617f9d7489fda949a2576928c84d2bff25825b
efeovez <efeovez2010@gmail.com>
2025-08-18 13:48:59 +0300

    add interface for staking

---
+++ contracts/staking/IStaking.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IERC20} from "../libs/IERC20.sol";
+
+interface IStaking is IERC20 {
+    
+    /* ================= FUNCTIONS ================= */
+
+    function createProvider(string memory description, uint8 commission) external;
+
+    function editProvider(string memory description, uint8 commission) external;
+
+    function delegate(address provider, uint256 amount) external;
+
+    function undelegate(address provider, uint256 amount) external;
+
+    function stake(uint256 amount) external;
+
+    function unstake(uint256 amount) external;
+
+    function getTotalReward() external view returns(uint256);
+
+    function calculateProviderReward(address provider) external view returns(uint256);
+
+    function calculateDelegatorReward(address delegator, address provider) external view returns(uint256);
+
+    function withdrawProviderReward() external;
+
+    function withdrawDelegatorReward(address provider) external;
+
+    function setLockPeriod(uint256 newLockPeriod) external;
+
+    function setMaxProviders(uint256 newMaxProviders) external;
+
+    function setBasis(IERC20 newBasis) external;
+
+    function setSbasis(IERC20 newSbasis) external;
+}
